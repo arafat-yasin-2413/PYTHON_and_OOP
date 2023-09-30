d7453ee6843c81e1b3ebb383b51c7bacfc5a3087
arafat-yasin-2413 <129652012+arafat-yasin-2413@users.noreply.github.com>
2023-09-30 16:54:44 +0600

    Update problem 5

--- Mod 4/5_problem.py
+++ Mod 4/5_problem.py
@@ -2,15 +2,22 @@ import pyautogui
 # from time import sleep
 import time
 
-time.sleep(2)
+
 
 n = int (input())
+time.sleep(5)
+
 for i in range(n):
     for j in range(i+1):
         # print('#',end='')
         pyautogui.write('#')
     # print()
     pyautogui.press('enter')
+#
+##
+###
+####
+#####
 
 
 
