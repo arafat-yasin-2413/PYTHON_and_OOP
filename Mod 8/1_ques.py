4be42a4a5c4a6f41b22b68049de3796a4214bb8c
arafat-yasin-2413 <129652012+arafat-yasin-2413@users.noreply.github.com>
2023-10-11 15:13:28 +0600

    first commit

--- Mod 8/1_ques.py
+++ Mod 8/1_ques.py
@@ -5,20 +5,26 @@ entry_hall() to insert an object of class Hall (Described below) inside
 its hall_list.
 '''
 
+class Hall:
+    def __init__(self,row,col,hall_no):
+        self.row = row
+        self.col = col
+        self.hall_no = hall_no
+        self.seats = {}
+        self.show_list = []
 
-class Star_Cinema:
+
+
+class  Star_Cinema:
     __hall_list = []
 
-    def entry_hall(self):
+    def __init__(self,row,col,hall_no):
+        super().__init__(row,col,hall_no)
         Star_Cinema.__hall_list.append(self)
 
-
-class Hall(Star_Cinema):
-    def __init__(self, row, col, hall_no):
-        super().__init__()
-        self.hall_list.append(self)
-        self.entry_hall(self)
+    def entry_hall(self):
+        pass
 
 
-object1 = Hall(5, 3, 2)
-# ei object ta k insert korte hobe
+
+Star_Cinema.entry_hall("")
