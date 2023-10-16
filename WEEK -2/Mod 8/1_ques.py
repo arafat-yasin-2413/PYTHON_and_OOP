5f7b3ad5f8075c5c532f9fd73ec4eb52c6890f52
arafat-yasin-2413 <129652012+arafat-yasin-2413@users.noreply.github.com>
2023-10-16 22:13:41 +0600

    first commit

--- WEEK -2/Mod 8/1_ques.py
+++ WEEK -2/Mod 8/1_ques.py
@@ -1,30 +1,73 @@
-'''
-Make a class named Star_Cinema which will have one class attribute
-named hall_list which is an empty list initially. Make a method named
-entry_hall() to insert an object of class Hall (Described below) inside
-its hall_list.
-'''
 
-class Hall:
-    def __init__(self,row,col,hall_no):
-        self.row = row
-        self.col = col
-        self.hall_no = hall_no
+class Star_Cinema:
+    __hall_list = []
+    def entry_hall(self,obj):
+        Star_Cinema.__hall_list.append(obj)
+
+    @classmethod
+    def print_hall_list(self):
+        for things in Star_Cinema.__hall_list:
+            print(things.rows, things.cols,things.hall_no)
+
+
+class Hall(Star_Cinema):
+    def __init__(self, rows, cols, hall_no):
+        super().__init__()
         self.seats = {}
         self.show_list = []
+        self.rows = rows
+        self.cols = cols
+        self.hall_no = hall_no
+        self.entry_hall(self)
+
+    def entry_show(self,iD,movie_name,tiMe):
+        self.iD = iD
+        self.movie_name = movie_name
+        self.tiMe = tiMe
+        make_tuple = (iD,movie_name,tiMe)
+        self.show_list.append(make_tuple)
+
+        self.seats[iD] = [ [0,0,0,0,0],
+                           [0,0,0,0,0],
+                           [0,0,0,0,0]
+                        ]
+
+        def book_seats(iD,lst):
+            pass
 
 
 
-class  Star_Cinema:
-    __hall_list = []
-
-    def __init__(self,row,col,hall_no):
-        super().__init__(row,col,hall_no)
-        Star_Cinema.__hall_list.append(self)
-
-    def entry_hall(self):
-        pass
 
 
 
-Star_Cinema.entry_hall("")
+
+
+object1 = Hall(3,5,2)
+# Star_Cinema.print_hall_list()
+object1.entry_show("MOVIE 1","Intersteller","11 am")
+object1.entry_show("MOVIE 2",'The Shawshank Redemption','2 pm')
+object1.entry_show("MOVIE 3",'Mission Impossible','5pm')
+# print(object1.show_list)
+
+# for movie in object1.show_list:
+    # print (movie)
+
+
+print(Star_Cinema.print_hall_list())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
