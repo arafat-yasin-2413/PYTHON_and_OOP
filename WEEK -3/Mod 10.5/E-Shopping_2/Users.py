b0732d7998012549b714fffabfbeb4e73c47db85
arafat-yasin-2413 <129652012+arafat-yasin-2413@users.noreply.github.com>
2023-11-14 01:02:34 +0600

    first commit

--- WEEK -3/Mod 10.5/E-Shopping_2/Users.py
+++ WEEK -3/Mod 10.5/E-Shopping_2/Users.py
@@ -3,128 +3,149 @@ Uml design
 
 https://drive.google.com/file/d/1hk_LYL1Q7_cg-D6-9NrOQD7EgcwudLaN/view?usp=sharing
 https://shorturl.at/bFLSV
+
+https://shorturl.at/dhL05
 '''
 
-
 from abc import ABC,abstractmethod
+
+
+''' -------------------------------------Product Segment------------------------------------------'''
+
+
+
+
+
+
+
+
+
+
+
+
+''' -------------------------------------Product Segment Ends------------------------------------------'''
+
+
 class User(ABC):
     user_list = []
-    def __init__(self,name,email,password,type):
+    def __init__(self,name,email,password):
         self.name = name
         self.email = email
         self.__password = password
-        self.type = type
 
-        User.user_list.append(self)
-        self.length = len(User.user_list)
+    def create_account(self,name,email,password,type):
+        if type.upper() == "CUSTOMER":
+            cust_obj = Customer(name, email, password,type)
+            User.user_list.append(cust_obj)
+            return cust_obj
 
+        elif type.upper() == "SELLER":
+            seller_obj = Seller(name, email, password,type)
+            User.user_list.append(seller_obj)
+            return seller_obj
 
     @abstractmethod
     def show_profile(self):
-        pass
+        raise NotImplementedError
 
-    @classmethod
-    def create_account(cls,name,email,password,type):
-        if type.upper() == 'CUSTOMER':
-            return Customer(name,email,password)
-        elif type.upper() == 'SELLER':
-            return Seller(name, email, password)
-
-    def show_all_user_based_on_type(self):
-        for user in User.user_list:
-            if user.type.upper() == 'SELLER':
-                user.show_profile()
-
-            elif user.type.upper() == 'CUSTOMER':
-                user.show_profile()
 
     def get_password(self):
         return self.__password
 
 
 class Customer(User):
-    customer_list = []
-    def __init__(self,name,email,password):
-        super().__init__(name, email, password,'customer')
-        Customer.customer_list.append(self)
+    def __init__(self,name,email,password,type):
+        super().__init__(name, email, password)
+        self.type = type
 
     def show_profile(self):
         print(f"Customer's name : {self.name}, email : {self.email}")
 
-    def show_customer_list(self):
-        for customer in Customer.customer_list:
-            customer.show_profile()
-
-
 class Seller(User):
-    seller_list = []
-    def __init__(self,name,email,password):
-        super().__init__(name, email, password,'seller')
-        Seller.seller_list.append(self)
-
+    def __init__(self,name,email,password,type):
+        super().__init__(name, email, password)
+        self.type = type
 
     def show_profile(self):
         print(f"Seller's name : {self.name}, email : {self.email}")
 
-    def show_seller_list(self):
-        for seller in Seller.seller_list:
-            seller.show_profile()
+
+class E_Shopping(User):
+    def __init__(self,name):
+        self.customer_list = []
+        self.name = name
+        self.seller_list = []
+
+    def show_profile(self):
+        print(f"This is E-Shopping site {self.name}")
+        print(f"Total no. sellers : {len(self.seller_list)}")
+        print(f"Total no. customers : {len(self.customer_list)}")
 
 
-#  --------------------------------------Product Segment starts ------------------------
+daraz = E_Shopping('Daraz Bd')
+
+# main starts
 
 
-
-
-#  --------------------------------------Product Segment ends ------------------------
-
-
-
-# main
-
-
-current_user = None
 while(True):
     print()
-    print('1. Register account ')
-    print('2. See total user ')
-    print('3. Login account ')
-    print('4. Exit ')
+    print('1. Create Account')
+    print('2. Login Account')
+    print('3. Show Sellers and Customers  ')
+    print('4. Show User List ')
+    print('5. Exit')
 
-    opt = int(input('Enter option : '))
-    if opt == 1:
-        # Registering
-        ch = input('Register as Seller/Customer (S/C) : ')
-        if ch.upper() == 'S':
-            # Registering as seller
-            print()
-            print('Registering as seller...')
+    ch = int(input('Enter your Choice : '))
+    if ch == 1:
+        account_type = input('Account type ? : ')
+
+        if account_type.upper() == 'SELLER':
+
+            print('Creating an account as Seller...')
             name = input('Enter name : ')
             email = input('Enter email : ')
-            password = input('Enter password : ')
-            Seller.create_account(name,email, password,'seller')
-            # Seller(name,email, password)
-            print(f"Successfully registered #{name} as a seller")
-            # print(f"total user : {len(User.user_list)}")
+            password = input('Enter Password : ')
+            s_obj = daraz.create_account(name, email, password,account_type)
+            daraz.seller_list.append(s_obj)
+            print(f'Successfully Registered as a Seller')
+            s_obj.show_profile()
 
-        elif ch.upper() == 'C':
-            # Registering as customer
-            print()
-            print('Registering as customer...')
+        elif account_type.upper() == "CUSTOMER":
+
+            print('Creating an account as Customer...')
             name = input('Enter name : ')
             email = input('Enter email : ')
-            password = input('Enter password : ')
-            Customer.create_account(name, email, password,'customer')
-            # Customer(name, email, password)
-            print(f"Successfully registered #{name} as a customer")
-
-    elif opt == 2:
-        print(f"total user : {len(User.user_list)}")
+            password = input('Enter Password : ')
+            c_obj = daraz.create_account(name, email, password, account_type)
+            daraz.customer_list.append(c_obj)
+            print(f'Successfully Registered as a Customer')
+            c_obj.show_profile()
 
 
-    elif opt == 3:
-        # Login account...
-        pass
+    elif ch == 2:
+        # Log in part
 
-    elif opt == 4:
+        email = input('Enter email : ')
+        password = input('Enter password : ')
+        for user in daraz.user_list:
+            # user.show_profile()
+            if user.email == email and user.get_password() == password:
+                print()
+                print(f'Welcome {user.name} and type : {user.type}')
+
+
+
+
+
+    elif ch == 3:
+        daraz.show_profile()
+
+    elif ch == 4:
+        # print(daraz.user_list)
+        for user in daraz.user_list:
+            user.show_profile()
+
+    elif ch == 5:
         break
+
+
